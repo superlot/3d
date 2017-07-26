9e3dd907ef731093e18d9e0181871afe93672a2c
superlot <mintolmax@gmail.com>
2017-07-26 10:49:34 +0400

    Update index.js

--- frame/js/index.js
+++ frame/js/index.js
@@ -48,7 +48,7 @@ $(document).ready(function() {
 		    return this.each(function(){
 		      var selectors = [
 		        "iframe[src^='https://superlot.github.io/3d/smart-car/smart-car.html']", 
-		      
+		       "iframe[src^='https://superlot.github.io']", 
 		        "object", 
 		        "embed"
 		      ];
